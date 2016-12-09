0d1d443f0b9170482c052112e73f4bfe663875e3
Jan Muehlemann <jan.muehlemann@gmail.com>
2016-12-09 08:56:55 +0100

    update example with lng toggle

--- example/app/components/View.js
+++ example/app/components/View.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { translate, Interpolate } from 'react-i18next';
 import AnotherComponent from './AnotherComponent';
 import YetAnotherComponent from './YetAnotherComponent';
-
+import i18n from '../i18n';
 
 @translate(['view', 'nav'], { wait: true })
 class TranslatableView extends React.Component {
@@ -10,10 +10,13 @@ class TranslatableView extends React.Component {
     const { t } = this.props;
 
     let interpolateComponent = <strong>"a interpolated component"</strong>;
+    const toggle = lng => i18n.changeLanguage(lng);
 
     return (
       <div>
         <h1>{t('common:appName')}</h1>
+        <button onClick={() => toggle('de')}>{t('nav:linkDE')}</button>
+        <button onClick={() => toggle('en')}>{t('nav:linkEN')}</button>
         <AnotherComponent />
         <YetAnotherComponent />
         <Interpolate parent='p' i18nKey='common:interpolateSample' value='"some value in props"' component={interpolateComponent} />
