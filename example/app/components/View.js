3484936ec5276287adc46cb9815b5109ff0dc339
Jan Muehlemann <jan.muehlemann@gmail.com>
2017-01-21 19:23:14 +0100

    extend sample to have a second component rendering delayed

--- example/app/components/View.js
+++ example/app/components/View.js
@@ -6,8 +6,23 @@ import i18n from '../i18n';
 
 @translate(['view', 'nav'], { wait: true })
 class TranslatableView extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ show: true });
+    }, 3000);
+  }
+
   render() {
     const { t } = this.props;
+    const { show } = this.state;
 
     let interpolateComponent = <strong>"a interpolated component"</strong>;
     const toggle = lng => i18n.changeLanguage(lng);
@@ -17,7 +32,9 @@ class TranslatableView extends React.Component {
         <h1>{t('common:appName')}</h1>
         <button onClick={() => toggle('de')}>{t('nav:linkDE')}</button>
         <button onClick={() => toggle('en')}>{t('nav:linkEN')}</button>
-        <AnotherComponent />
+        {
+          show && <AnotherComponent />
+        }
         <YetAnotherComponent />
         <Interpolate parent='p' i18nKey='common:interpolateSample' value='"some value in props"' component={interpolateComponent} />
         <Interpolate parent='p' i18nKey='common:interpolateSample' useDangerouslySetInnerHTML={true} value='"some value in props"' component={interpolateComponent} />
