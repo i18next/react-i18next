7b6d49f61d920667e6e25d1717f85f42f68ad7fb
Jan Muehlemann <jan.muehlemann@gmail.com>
2017-03-24 14:58:00 +0100

    fix webpack1 sample

--- example/webpack1/webpack.config.js
+++ example/webpack1/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['babel'],
+      loader: 'babel-loader',
       include: path.join(__dirname, 'app')
     }]
   }
