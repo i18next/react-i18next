adb4fcde4743d93f04bc44d059f72624466b937e
Jan Muehlemann <jan.muehlemann@gmail.com>
2017-05-15 11:11:28 +0200

    extend loadns test

--- __tests__/loadNamespaces-test.js
+++ __tests__/loadNamespaces-test.js
@@ -1,4 +1,5 @@
-jest.unmock('../src/translate');
+import React from 'react';
+jest.unmock('../src/loadNamespaces');
 
 import loadNamespaces from '../src/loadNamespaces';
 
@@ -19,13 +20,18 @@ describe('translate', () => {
     C1.namespaces = ['ns1'];
     function C2() {}
     C2.namespaces = ['ns21', 'ns22'];
-    const components = [C1, C2];
+    const C3 = React.createFactory('Elem');
+    C3.namespaces = ['ns31'];
+    const C4 = { element: { namespaces: ['ns41'] } }
+    const components = [C1, C2, C3, C4];
     const promise = loadNamespaces({ components, i18n });
     promise.then((data) => {
-      expect(data.length).toBe(3);
+      expect(data.length).toBe(5);
       expect(data[0]).toBe('ns1');
       expect(data[1]).toBe('ns21');
       expect(data[2]).toBe('ns22');
+      expect(data[3]).toBe('ns31');
+      expect(data[4]).toBe('ns41');
       done();
     });
   });
