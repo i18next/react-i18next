0146198fabdca76a8e8f4be0dd69fb15317c74e5
Andrew Branch <andrew@wheream.io>
2017-11-01 15:16:14 -0700

    Add failing test for initial I18n ready state

---
+++ test/I18n.spec.js
@@ -0,0 +1,32 @@
+jest.unmock('../src/I18n');
+import I18n from '../src/I18n';
+
+const i18n = {
+  options: {
+    defaultNS: 'defaultNS'
+  },
+  services: {
+    resourceStore: {
+      data: {}
+    }
+  },
+  changeLanguage: () => {},
+  getFixedT(p, ns) {
+    return message => message;
+  },
+  hasResourceBundle(lng, ns) {
+    return ns === 'alreadyLoadedNS';
+  }
+};
+
+describe('I18n', () => {
+  it('sets initial ready state to false if resource bundles are not loaded', () => {
+    const notLoadedInstance = new I18n({ i18n, ns: ['notLoadedNS', 'alreadyLoadedNS'] }, {});
+    expect(notLoadedInstance.state.ready).toBe(false);
+  });
+
+  it('sets initial ready state to true if resource bundles are already loaded', () => {
+    const notLoadedInstance = new I18n({ i18n, ns: ['alreadyLoadedNS'] }, {});
+    expect(notLoadedInstance.state.ready).toBe(true);
+  });
+});
