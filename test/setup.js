d542374c84df114ffc03db9b80fa41eb7158216a
Mark Langovoi <marklangovoi@gmail.com>
2018-04-01 21:43:26 +0300

    Improve tests system
    Add enzyme-adapter-react-helper to support multiple versions of React (0.14, 15, 16)

--- test/setup.js
+++ test/setup.js
@@ -1,7 +1,5 @@
 import 'raf/polyfill';
 
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import configure from 'enzyme-adapter-react-helper';
 
-
-Enzyme.configure({ adapter: new Adapter() });
+configure();
