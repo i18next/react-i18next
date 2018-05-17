3b55dda62696f5ab82938703c9f2900cc32b1cfe
Jan Muehlemann <jan.muehlemann@gmail.com>
2018-05-17 08:43:11 +0200

    more mocking

--- example/test-jest/__mocks__/react-i18next.js
+++ example/test-jest/__mocks__/react-i18next.js
@@ -1,7 +1,19 @@
 const React = require('react');
+const reactI18next = require('react-i18next');
 
 module.exports = {
   // this mock makes sure any components using the translate HoC receive the t function as a prop
-  translate: () => Component => props => <Component t={(k) => k} {...props} />,
-  Trans: ({ children }) => children
+  translate: () => Component => props => <Component t={k => k} {...props} />,
+  Trans: ({ children }) => children,
+  I18n: ({ children }) => children(k => k, { i18n: {} }),
+
+  // mock if needed
+  Interpolate: reactI18next.Interpolate,
+  I18nextProvider: reactI18next.I18nextProvider,
+  loadNamespaces: reactI18next.loadNamespaces,
+  reactI18nextModule: reactI18next.reactI18nextModule,
+  setDefaults: reactI18next.setDefaults,
+  getDefaults: reactI18next.getDefaults,
+  setI18n: reactI18next.setI18n,
+  getI18n: reactI18next.getI18n
 };
