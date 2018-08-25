b702d54a2e4416e1cf7995fb86c367d69896d019
Jan Muehlemann <jan.muehlemann@gmail.com>
2018-08-25 17:35:15 +0200

    update mock

--- example/test-jest/__mocks__/react-i18next.js
+++ example/test-jest/__mocks__/react-i18next.js
@@ -1,11 +1,43 @@
 const React = require('react');
 const reactI18next = require('react-i18next');
 
+const hasChildren = node =>
+  node && (node.children || (node.props && node.props.children));
+
+const getChildren = node =>
+  (node && node.children ? node.children : node.props && node.props.children);
+
+const renderNodes = (reactNodes) => {
+  if (typeof reactNodes === 'string') {
+    return reactNodes;
+  }
+
+  return Object.keys(reactNodes).map((key, i) => {
+    const child = reactNodes[key];
+    const isElement = React.isValidElement(child);
+
+    if (typeof child === 'string') {
+      return child;
+    } else if (hasChildren(child)) {
+      const inner = renderNodes(getChildren(child));
+      return React.cloneElement(
+        child,
+        { ...child.props, key: i },
+        inner,
+      );
+    } else if (typeof child === 'object' && !isElement) {
+      return Object.keys(child).reduce((str, childKey) => `${str}${child[childKey]}`, '');
+    }
+
+    return child;
+  });
+};
+
 module.exports = {
   // this mock makes sure any components using the translate HoC receive the t function as a prop
   translate: () => Component => props => <Component t={k => k} {...props} />,
-  Trans: ({ children }) => children,
-  I18n: ({ children }) => children(k => k, { i18n: {} }),
+  Trans: ({ children }) => renderNodes(children),
+  I18n: ({ children }) => children(k => k, { i18n: {} }),
 
   // mock if needed
   Interpolate: reactI18next.Interpolate,
@@ -15,5 +47,5 @@ module.exports = {
   setDefaults: reactI18next.setDefaults,
   getDefaults: reactI18next.getDefaults,
   setI18n: reactI18next.setI18n,
-  getI18n: reactI18next.getI18n
+  getI18n: reactI18next.getI18n,
 };
