1712f0978456f79b42773ccc2c778bc680425b9b
Jan Muehlemann <jan.muehlemann@gmail.com>
2018-10-31 11:55:21 +0100

    allow init of ssr to have lng in different place than store

--- src/hooks/useSSR.js
+++ src/hooks/useSSR.js
@@ -1,17 +1,21 @@
 import { getI18n } from './context';
 
-let initializedOnce = false;
+let initializedLanguageOnce = false;
+let initializedStoreOnce = false;
 
 export function useSSR(initialI18nStore, initialLanguage) {
   // only set this once
-  if (initializedOnce) return;
+  if (initializedLanguageOnce && initializedStoreOnce) return;
 
   const i18n = getI18n();
 
   // nextjs / SSR: getting data from next.js or other ssr stack
-  if (initialI18nStore && initialLanguage) {
+  if (initialI18nStore && !initializedStoreOnce) {
     i18n.services.resourceStore.data = initialI18nStore;
+    initializedStoreOnce = true;
+  }
+  if (initialLanguage && !initializedLanguageOnce) {
     i18n.changeLanguage(initialLanguage);
-    initializedOnce = true;
+    initializedLanguageOnce = true;
   }
 }
