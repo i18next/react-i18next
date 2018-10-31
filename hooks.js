e8bf3229017c4953f0a0bdcc5c100777a1eaaa47
Jan Muehlemann <jan.muehlemann@gmail.com>
2018-10-31 11:19:20 +0100

    expose new fc

--- hooks.js
+++ hooks.js
@@ -1,10 +1,14 @@
 export {
   useTranslation,
   withTranslation,
+  useSSR,
+  withSSR,
   Trans,
   initReactI18n,
   setDefaults,
   getDefaults,
   setI18n,
   getI18n,
+  composeInitialProps,
+  getInitialProps,
 } from './dist/es/hooks';
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,15 @@
 export { Trans } from './Trans';
 export { useTranslation } from './useTranslation';
 export { withTranslation } from './withTranslation';
+export { withSSR } from './withSSR';
+export { useSSR } from './useSSR';
 
-export { initReactI18n, setDefaults, getDefaults, setI18n, getI18n } from './context';
+export {
+  initReactI18n,
+  setDefaults,
+  getDefaults,
+  setI18n,
+  getI18n,
+  composeInitialProps,
+  getInitialProps,
+} from './context';
