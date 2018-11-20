a8a200e53518778d2465d04de9d501df7f77a575
Dung Truong <checkraiser11@gmail.com>
2018-11-20 16:49:26 +0700

    Add share config

---
+++ example/nextjs/lib/share-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  defaultLanguage: 'en',
+  otherLanguages: ['de'],
+  defaultNamespace: 'common',
+  localePath: 'static/locales',
+  localeStructure: '{{lng}}/{{ns}}',
+  localeSubpaths: false,
+}
