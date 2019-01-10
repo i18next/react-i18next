5b532536736f2cae5930c117eea17b1c2cbb9799
tkow <t.kusama0613@outlook.jp>
2019-01-10 14:43:27 +0900

    chane inteface for TranslationFunction structured

--- src/index.d.ts
+++ src/index.d.ts
@@ -14,11 +14,6 @@ interface ReactI18nextModule {
   init: (instance: i18next.i18n) => void;
 }
 
-interface ReactI18nextTranslateFunction {
-  t<TKeys extends string=string,TVals extends object =object,TResult=any>
-  (key: TKeys | TKeys[], options?: i18next.TranslationOptions<TVals>):TResult;
-}
-
 export const reactI18nextModule: ReactI18nextModule;
 
 export function setDefaults(options: ReactI18NextOptions): void;
@@ -29,7 +24,7 @@ export function setI18n(instance: i18next.i18n): void;
 
 export function getI18n(): i18next.i18n;
 
-export interface I18nContextValues extends ReactI18nextTranslateFunction{
+export interface I18nContextValues extends i18next.WithT {
   i18n: i18next.i18n;
   defaultNS?: string;
   reportNS?: string;
@@ -80,7 +75,7 @@ export interface NamespacesConsumerProps extends ReactI18NextOptions {
   initialI18nStore?: {};
   initialLanguage?: string;
   children: (
-    t: ReactI18nextTranslateFunction['t'],
+    t: i18next.TranslationFunction,
     options: {
       i18n: i18next.i18n;
       lng: string;
@@ -100,7 +95,7 @@ export interface I18nextProviderProps {
 
 export const I18nextProvider: React.ComponentClass<I18nextProviderProps>;
 
-export interface TransProps extends Partial<ReactI18nextTranslateFunction>{
+export interface TransProps extends Partial<i18next.WithT>{
   i18nKey?: string;
   count?: number;
   parent?: React.ReactNode;
