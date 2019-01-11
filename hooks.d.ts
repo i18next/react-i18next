8a9d2c58708d46c577b346b17ed722b9b99042bd
Kevin Ross <kevin.ross@alienfast.com>
2019-01-11 14:03:38 -0500

    Sync typescript setup with i18next using dtslint and prettier (no other code changes)

--- hooks.d.ts
+++ hooks.d.ts
@@ -1,65 +1,63 @@
-import i18next from "i18next";
-import * as React from "react";
-import {Namespace, ReactI18NextOptions, TransProps} from "./src/index";
+import i18next from 'i18next'
+import * as React from 'react'
+import { Namespace, ReactI18NextOptions, TransProps } from './src/index'
 
 interface HooksTransProps extends TransProps {
-    children: React.ReactElement<any>[];
-    tOptions: {};
-    ns: Namespace;
+  children: React.ReactElement<any>[]
+  tOptions: {}
+  ns: Namespace
 }
-export function setDefaults(options: ReactI18NextOptions): void;
-export function getDefaults(): ReactI18NextOptions;
-export function addUsedNamespaces(namespaces: Namespace[]): void;
-export function getUsedNamespaces(): string[];
-export function setI18n(instance: i18next.i18n): void;
-export function getI18n(): i18next.i18n;
+export function setDefaults(options: ReactI18NextOptions): void
+export function getDefaults(): ReactI18NextOptions
+export function addUsedNamespaces(namespaces: Namespace[]): void
+export function getUsedNamespaces(): string[]
+export function setI18n(instance: i18next.i18n): void
+export function getI18n(): i18next.i18n
 export const initReactI18next: {
-    type: string;
-    init(instance: i18next.i18n): void;
-};
-export function composeInitialProps(ForComponent: any): (ctx: unknown) => Promise<any>;
+  type: string
+  init(instance: i18next.i18n): void
+}
+export function composeInitialProps(ForComponent: any): (ctx: unknown) => Promise<any>
 export function getInitialProps(): {
-    initialI18nStore: {
-        [ns: string]: {};
-    };
-    initialLanguage: string;
-};
+  initialI18nStore: {
+    [ns: string]: {}
+  }
+  initialLanguage: string
+}
 export function Trans({
-    i18nKey,
-    count,
-    parent,
-    i18n: i18nFromProps,
-    t: tFromProps,
-    defaults,
-    values,
-    components,
-    children,
-    tOptions,
-    ns,
-    ...additionalProps
-}: HooksTransProps): any;
+  i18nKey,
+  count,
+  parent,
+  i18n: i18nFromProps,
+  t: tFromProps,
+  defaults,
+  values,
+  components,
+  children,
+  tOptions,
+  ns,
+  ...additionalProps
+}: HooksTransProps): any
 
-export function useSSR(initialI18nStore: any, initialLanguage: any): void;
-export function useTranslation(
-    ns?: Namespace,
-): [i18next.TranslationFunction, i18next.i18n | {}];
+export function useSSR(initialI18nStore: any, initialLanguage: any): void
+export function useTranslation(ns?: Namespace): [i18next.TranslationFunction, i18next.i18n | {}]
 
 export function withSSR(): (
-    WrappedComponent: React.ComponentClass<{}, any>,
+  WrappedComponent: React.ComponentClass<{}, any>,
 ) => {
-    (
-        {
-            initialI18nStore,
-            initialLanguage,
-            ...rest
-        }: {
-            [x: string]: any;
-            initialI18nStore: any;
-            initialLanguage: any;
-        },
-    ): React.ComponentElement<{}, React.Component<{}, any, any>>;
-    getInitialProps: (ctx: unknown) => Promise<any>;
-};
+  (
+    {
+      initialI18nStore,
+      initialLanguage,
+      ...rest
+    }: {
+      [x: string]: any
+      initialI18nStore: any
+      initialLanguage: any
+    },
+  ): React.ComponentElement<{}, React.Component<{}, any, any>>
+  getInitialProps: (ctx: unknown) => Promise<any>
+}
 export function withTranslation(
-    ns?: Namespace,
-): (WrappedComponent: React.ComponentClass<any>) => (props: any) => any;
+  ns?: Namespace,
+): (WrappedComponent: React.ComponentClass<any>) => (props: any) => any
