a87034184a16966b311125dc5af62b09a90030ac
Kevin Ross <kevin.ross@alienfast.com>
2019-01-11 13:11:10 -0600

    Sync typescript setup with i18next using dtslint and prettier (no other code changes) (#672)

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,110 +1,108 @@
-import * as React from "react";
-import i18next from "i18next";
+import * as React from 'react'
+import i18next from 'i18next'
 
-type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
-type Subtract<T, K> = Omit<T, keyof K>;
+type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
+type Subtract<T, K> = Omit<T, keyof K>
 
 export interface ReactI18NextOptions extends i18next.ReactOptions {
-  usePureComponent?: boolean;
-  omitBoundRerender?: boolean;
+  usePureComponent?: boolean
+  omitBoundRerender?: boolean
 }
 
 interface ReactI18nextModule {
-  type: string;
-  init: (instance: i18next.i18n) => void;
+  type: string
+  init: (instance: i18next.i18n) => void
 }
 
-export const reactI18nextModule: ReactI18nextModule;
+export const reactI18nextModule: ReactI18nextModule
 
-export function setDefaults(options: ReactI18NextOptions): void;
+export function setDefaults(options: ReactI18NextOptions): void
 
-export function getDefaults(): ReactI18NextOptions;
+export function getDefaults(): ReactI18NextOptions
 
-export function setI18n(instance: i18next.i18n): void;
+export function setI18n(instance: i18next.i18n): void
 
-export function getI18n(): i18next.i18n;
+export function getI18n(): i18next.i18n
 
 export interface I18nContextValues {
-  i18n: i18next.i18n;
-  t: i18next.TranslationFunction;
-  defaultNS?: string;
-  reportNS?: string;
-  lng?: string;
+  i18n: i18next.i18n
+  t: i18next.TranslationFunction
+  defaultNS?: string
+  reportNS?: string
+  lng?: string
 }
 
-export const I18nContext: React.Context<I18nContextValues>;
+export const I18nContext: React.Context<I18nContextValues>
 
 export interface WithI18n extends I18nContextValues {
-  i18nOptions?: ReactI18NextOptions;
+  i18nOptions?: ReactI18NextOptions
 }
 
 export function withI18n(): <P extends object>(
-  Wrapper: React.ComponentType<P>
-) => React.ComponentType<Subtract<P, WithI18n>>;
+  Wrapper: React.ComponentType<P>,
+) => React.ComponentType<Subtract<P, WithI18n>>
 
 export interface WithNamespaces extends WithI18n {
-  tReady: boolean;
-  initialI18nStore?: {};
-  initialLanguage?: string;
+  tReady: boolean
+  initialI18nStore?: {}
+  initialLanguage?: string
 }
 
 export interface WithNamespacesOptions extends ReactI18NextOptions {
-  wait: boolean;
-  i18n?: i18next.i18n;
-  innerRef?:
-    | ((instance: any) => void)
-    | React.RefObject<HTMLElement | SVGElement | React.Component>;
+  wait: boolean
+  i18n?: i18next.i18n
+  innerRef?: ((instance: any) => void) | React.RefObject<HTMLElement | SVGElement | React.Component>
 }
 
-type Namespace = string | string[];
+type Namespace = string | string[]
 interface NamespaceExtractor {
-  (props: any & { namespace: Namespace }): Namespace;
+  (props: any & { namespace: Namespace }): Namespace
 }
 
 export function withNamespaces(
   namespace?: Namespace | NamespaceExtractor,
-  options?: WithNamespacesOptions
+  options?: WithNamespacesOptions,
 ): <P extends WithNamespaces>(
-  component: React.ComponentType<P>
-) => React.ComponentType<Subtract<P, WithNamespaces>>;
+  component: React.ComponentType<P>,
+) => React.ComponentType<Subtract<P, WithNamespaces>>
 
-export const translate: typeof withNamespaces;
+export const translate: typeof withNamespaces
 
 export interface NamespacesConsumerProps extends ReactI18NextOptions {
-  ns?: Namespace;
-  i18n?: i18next.i18n;
-  initialI18nStore?: {};
-  initialLanguage?: string;
+  ns?: Namespace
+  i18n?: i18next.i18n
+  initialI18nStore?: {}
+  initialLanguage?: string
   children: (
     t: i18next.TranslationFunction,
     options: {
-      i18n: i18next.i18n;
-      lng: string;
-      ready: boolean;
-    }
-  ) => React.ReactNode;
+      i18n: i18next.i18n
+      lng: string
+      ready: boolean
+    },
+  ) => React.ReactNode
 }
 
-export const NamespacesConsumer: React.ComponentClass<NamespacesConsumerProps>;
+export const NamespacesConsumer: React.ComponentClass<NamespacesConsumerProps>
 
 export interface I18nextProviderProps {
-  i18n: i18next.i18n;
-  defaultNS?: string;
-  initialI18nStore?: {};
-  initialLanguage?: string;
+  i18n: i18next.i18n
+  defaultNS?: string
+  initialI18nStore?: {}
+  initialLanguage?: string
 }
 
-export const I18nextProvider: React.ComponentClass<I18nextProviderProps>;
+export const I18nextProvider: React.ComponentClass<I18nextProviderProps>
 
 export interface TransProps {
-  i18nKey?: string;
-  count?: number;
-  parent?: React.ReactNode;
-  i18n?: i18next.i18n;
-  t?: i18next.TranslationFunction;
-  defaults?: string;
-  values?: {};
-  components?: React.ReactNode[];
+  i18nKey?: string
+  count?: number
+  parent?: React.ReactNode
+  i18n?: i18next.i18n
+  t?: i18next.TranslationFunction
+  defaults?: string
+  values?: {}
+  components?: React.ReactNode[]
 }
 
-export const Trans: React.ComponentClass<TransProps>;
+export const Trans: React.ComponentClass<TransProps>
--- test/typescript/examples-react.test.tsx
+++ test/typescript/examples-react.test.tsx
@@ -1,20 +1,11 @@
-import * as React from "react";
-import {
-  NamespacesConsumer,
-  Trans,
-  withNamespaces,
-  WithNamespaces,
-} from "../../src/index";
+import * as React from 'react'
+import { NamespacesConsumer, Trans, withNamespaces, WithNamespaces } from 'react-i18next'
 
 // Component using the render prop NamespacesConsumer
 // get t function inside the component
 // learn more: https://react.i18next.com/components/namespacesconsumer
 function Welcome() {
-  return (
-    <NamespacesConsumer>
-      {(t, { i18n }) => <h2>{t("title")}</h2>}
-    </NamespacesConsumer>
-  );
+  return <NamespacesConsumer>{(t, { i18n }) => <h2>{t('title')}</h2>}</NamespacesConsumer>
 }
 
 // Component using the higher order component withNamespaces
@@ -25,34 +16,34 @@ function MyComponent({ t }: WithNamespaces) {
     <Trans i18nKey="description.part1">
       To get started, edit <code>src/App.js</code> and save to reload.
     </Trans>
-  );
+  )
 }
-const MyComponentWrapped = withNamespaces()(MyComponent);
+const MyComponentWrapped = withNamespaces()(MyComponent)
 
 // the app gets passed in t and i18n by using same hoc withNamespaces
 // using i18n.changeLanguage you can change the language programmatically
 // (same is possible using the NamespacesConsumer render prop - just read the docs)
 class App extends React.Component<WithNamespaces> {
-  public render() {
-    const { t, i18n } = this.props;
+  render() {
+    const { t, i18n } = this.props
 
     const changeLanguage = (lng: string) => {
-      i18n.changeLanguage(lng);
-    };
+      i18n.changeLanguage(lng)
+    }
 
     return (
       <div className="App">
         <div className="App-header">
           <Welcome />
-          <button onClick={() => changeLanguage("de")}>de</button>
-          <button onClick={() => changeLanguage("en")}>en</button>
+          <button onClick={() => changeLanguage('de')}>de</button>
+          <button onClick={() => changeLanguage('en')}>en</button>
         </div>
         <div className="App-intro">
           <MyComponentWrapped />
         </div>
-        <div>{t("description.part2")}</div>
+        <div>{t('description.part2')}</div>
       </div>
-    );
+    )
   }
 }
-export default withNamespaces("translation")(App);
+export default withNamespaces('translation')(App)
