57d23cb09b3fcd6c12837906433100bebc4c25d8
Jan Muehlemann <jan.muehlemann@gmail.com>
2019-01-30 21:56:13 +0100

    update sample

--- example/react/src/i18n.js
+++ example/react/src/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import Backend from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import { initReactI18next } from 'react-i18next/hooks';
+import { initReactI18next } from 'react-i18next';
 
 i18n
   // load translation using xhr -> see /public/locales
