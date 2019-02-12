dc765902d1a659c65a16ee47369d8b6d7304b32e
gasolin <gasolin@gmail.com>
2019-02-12 15:19:13 +0800

    add withNamespaces mock

--- example/test-jest/__mocks__/react-i18next.js
+++ example/test-jest/__mocks__/react-i18next.js
@@ -34,7 +34,8 @@ useMock.t = k => k;
 useMock.i18n = {};
 
 module.exports = {
-  // this mock makes sure any components using the translate HoC receive the t function as a prop
+  // withNamespaces/withTranslation mocks make sure any components using the translate HoC receive the t function as a prop
+  withNamespaces: () => Component => props => <Component t={k => k} {...props} />,
   withTranslation: () => Component => props => <Component t={k => k} {...props} />,
   Trans: ({ children }) => renderNodes(children),
   Translation: ({ children }) => children(k => k, { i18n: {} }),
