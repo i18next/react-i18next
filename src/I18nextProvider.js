08197764ad4bef1e3b8b9b1d0408c7adcfb1cbe9
keustma <mathias.keustermans@gmail.com>
2019-06-17 18:03:52 +0200

    Update I18nextProvider.js

--- src/I18nextProvider.js
+++ src/I18nextProvider.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { I18nContext, usedI18nextProvider } from './context';
 
+const _warnRemovedContextProp = prop => {
+  console.warn(
+    `[react-i18next] I18nextProvider no longer provides "${prop}" as a context prop, please upgrade your code: https://react.i18next.com/latest/migrating-v9-to-v10`
+  );
+};
+
 export function I18nextProvider({ i18n, defaultNS, children }) {
   usedI18nextProvider(true);
 
@@ -10,6 +16,9 @@ export function I18nextProvider({ i18n, defaultNS, children }) {
       value: {
         i18n,
         defaultNS,
+        reportNS: _warnRemovedContextProp('reportNS'),
+        lng: _warnRemovedContextProp('lng'),
+        t: _warnRemovedContextProp('t')
       },
     },
     children,
