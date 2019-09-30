d93bb6a6acf15232c371782a12cdac986c5fb4d1
Kevin Ross <kevin.ross@alienfast.com>
2019-09-30 11:01:41 -0500

    Test exposed i18n types (#956)
    * omit ts from eslint (until we configure it)
    
    * Ensure exposed i18n types exist
    
    Test/assert for #954
    
    * make sure it is tested for both interop variations

--- test/typescript/useTranslation.test.tsx
+++ test/typescript/useTranslation.test.tsx
@@ -3,6 +3,11 @@ import { useTranslation } from 'react-i18next';
 
 function defaultObjectUsage() {
   const { t, i18n } = useTranslation();
+
+  // ensure the i18n here is still exposing original type
+  const language = i18n.language;
+  i18n.languages.join(', ');
+
   return (
     <div>
       {t('key1')} {i18n.exists('key1')}
