4cf890ebf23e40802683b3a4be22b62bd0e9d435
Ryan Swanson <ryan.swanson@theice.com>
2020-01-14 16:40:55 -0500

    Added TypeScript tests of the 'ready' return value from useTranslation and the 'tReady' prop from withTranslation/WithTranslation.

--- test/typescript/useTranslation.test.tsx
+++ test/typescript/useTranslation.test.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 
 function defaultObjectUsage() {
-  const { t, i18n } = useTranslation();
+  const { t, i18n, ready } = useTranslation();
 
   // ensure the i18n here is still exposing original type
   const language = i18n.language;
   i18n.languages.join(', ');
+  const r: boolean = ready;
 
   return (
     <div>
@@ -16,7 +17,8 @@ function defaultObjectUsage() {
 }
 
 function alternateArrayUsage() {
-  const [t, i18n] = useTranslation();
+  const [t, i18n, ready] = useTranslation();
+  const r: boolean = ready;
   return (
     <div>
       {t('key1')} {i18n.exists('key1')}
