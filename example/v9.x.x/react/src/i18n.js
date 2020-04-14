800538babff7fffb10ce2a04af6684b30de8c3fa
Adriano Raiano <adrai@users.noreply.github.com>
2020-04-14 16:44:36 +0200

    replace xhr backend with new http backend (#1084)

--- example/v9.x.x/react/src/i18n.js
+++ example/v9.x.x/react/src/i18n.js
@@ -1,11 +1,11 @@
 import i18n from 'i18next';
-import Backend from 'i18next-xhr-backend';
+import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { reactI18nextModule } from 'react-i18next';
 
 i18n
-  // load translation using xhr -> see /public/locales
-  // learn more: https://github.com/i18next/i18next-xhr-backend
+  // load translation using http -> see /public/locales
+  // learn more: https://github.com/i18next/i18next-http-backend
   .use(Backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
--- example/v9.x.x/test-jest/src/i18n.js
+++ example/v9.x.x/test-jest/src/i18n.js
@@ -1,5 +1,5 @@
 import i18n from 'i18next';
-import Backend from 'i18next-xhr-backend';
+import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { reactI18nextModule } from 'react-i18next';
 
