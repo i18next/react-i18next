6001a316f7b93f76ed2970c94ff1ebfe8eb714a3
Adriano Raiano <adriano@raiano.ch>
2020-05-18 11:05:05 +0200

    import i18next-http-backend/cjs for razzle #1116

--- example/razzle-ssr/src/i18n.js
+++ example/razzle-ssr/src/i18n.js
@@ -1,6 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
+import Backend from 'i18next-http-backend/cjs';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 const options = {
