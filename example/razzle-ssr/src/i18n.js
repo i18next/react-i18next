e3544fb3ec56bb3e9a3f043f086d7f6800949db3
Adriano Raiano <adriano@raiano.ch>
2020-05-21 09:47:02 +0200

    no need to require i18next-http-backend/cjs anymore

--- example/razzle-ssr/src/i18n.js
+++ example/razzle-ssr/src/i18n.js
@@ -1,6 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend/cjs';
+import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 const options = {
