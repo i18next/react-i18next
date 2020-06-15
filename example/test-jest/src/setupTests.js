d8c7bba381dcb8cdac9339ff62762fed5d2b581e
Adriano Raiano <adriano@raiano.ch>
2020-06-15 08:12:11 +0200

    test-jest with stubbing

--- example/test-jest/src/setupTests.js
+++ example/test-jest/src/setupTests.js
@@ -1,24 +1,24 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+// import i18n from 'i18next';
+// import { initReactI18next } from 'react-i18next';
 
-i18n.use(initReactI18next).init({
-  lng: 'en',
-  fallbackLng: 'en',
+// i18n.use(initReactI18next).init({
+//   lng: 'en',
+//   fallbackLng: 'en',
 
-  // have a common namespace used around the full app
-  ns: ['translations'],
-  defaultNS: 'translations',
+//   // have a common namespace used around the full app
+//   ns: ['translations'],
+//   defaultNS: 'translations',
 
-  // debug: true,
+//   // debug: true,
 
-  interpolation: {
-    escapeValue: false, // not needed for react!!
-  },
+//   interpolation: {
+//     escapeValue: false, // not needed for react!!
+//   },
 
-  resources: { en: { translations: {} } },
-});
+//   resources: { en: { translations: {} } },
+// });
 
 Enzyme.configure({ adapter: new Adapter() });
