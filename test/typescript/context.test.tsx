495c276c51a90f601b91a7dfb5e144c187151b3c
Sebastien GREGOIRE <sebastien.gregoire@ibm.com>
2020-06-29 15:03:27 +0200

    Fixed tests

--- test/typescript/context.test.tsx
+++ test/typescript/context.test.tsx
@@ -1,4 +1,39 @@
-import i18next from 'i18next';
+import i18n from './i18n';
 
-i18next.reportNamespaces.addUsedNamespaces(['translation']);
-i18next.reportNamespaces.getUsedNamespaces();
+// All keys should be here
+i18n.addResource;
+i18n.addResourceBundle;
+i18n.addResources;
+i18n.changeLanguage;
+i18n.cloneInstance;
+i18n.createInstance;
+i18n.dir;
+i18n.emit;
+i18n.exists;
+i18n.format;
+i18n.getDataByLanguage;
+i18n.getFixedT;
+i18n.getResource;
+i18n.getResourceBundle;
+i18n.hasResourceBundle;
+i18n.init;
+i18n.isInitialized;
+i18n.language;
+i18n.languages;
+i18n.loadLanguages;
+i18n.loadNamespaces;
+i18n.loadResources;
+i18n.modules;
+i18n.off;
+i18n.on;
+i18n.options;
+i18n.reloadResources;
+i18n.removeResourceBundle;
+i18n.services;
+i18n.setDefaultNamespace;
+i18n.store;
+i18n.t;
+i18n.use;
+// To be reviewed: No reference found in official documentation.
+// i18next.reportNamespaces.addUsedNamespaces(['translation']);
+// i18next.reportNamespaces.getUsedNamespaces();
