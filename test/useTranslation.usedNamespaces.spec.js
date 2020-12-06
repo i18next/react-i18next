3f683964e6642492c4b89632e83447e6412fc18b
Tiger Abrodi <tigerabrodi@gmail.com>
2020-12-06 18:22:13 +0100

    useTranslation.usedNamespaces.spec.js, 1 test

--- test/useTranslation.usedNamespaces.spec.js
+++ test/useTranslation.usedNamespaces.spec.js
@@ -1,37 +1,30 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { renderHook } from '@testing-library/react-hooks';
 import { useTranslation } from '../src/useTranslation';
 
 jest.unmock('../src/useTranslation');
 
-const i18n = {
-  language: 'en',
-  languages: ['en'],
-  services: {
-    resourceStore: {
-      data: {},
-    },
-    backendConnector: { backend: {} },
-  },
-  isInitialized: true,
-  changeLanguage: () => {},
-  getFixedT: () => message => message,
-  hasResourceBundle: (lng, ns) => ns === 'ns1' || ns === 'ns2' || ns === 'ns3',
-  loadNamespaces: () => {},
-  on: () => {},
-  options: {},
-};
-
 describe('useTranslation', () => {
-  function TestComponent() {
-    const [t] = useTranslation(['ns1', 'ns2', 'ns3'], { i18n });
-
-    return <div>{t('keyOne')}</div>;
-  }
-
   it('should render correct content if ready (having all ns)', () => {
-    mount(<TestComponent />, {});
-    // console.log(wrapper.debug());
+    const i18n = {
+      language: 'en',
+      languages: ['en'],
+      services: {
+        resourceStore: {
+          data: {},
+        },
+        backendConnector: { backend: {} },
+      },
+      isInitialized: true,
+      changeLanguage: () => {},
+      getFixedT: () => message => message,
+      hasResourceBundle: (lng, ns) => ns === 'ns1' || ns === 'ns2' || ns === 'ns3',
+      loadNamespaces: () => {},
+      on: () => {},
+      off: () => {},
+      options: {},
+    };
+    renderHook(() => useTranslation(['ns1', 'ns2', 'ns3'], { i18n }));
     expect(i18n.reportNamespaces.getUsedNamespaces()).toEqual(['ns1', 'ns2', 'ns3']);
   });
 });
