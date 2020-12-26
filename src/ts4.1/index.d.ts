a0c7458892cbf8d3be7a0958850967087b420d1b
Pedro Durek <pedro_hmd@hotmail.com>
2020-12-26 15:45:11 -0600

    Fix inference for specific keys (#1215)

--- src/ts4.1/index.d.ts
+++ src/ts4.1/index.d.ts
@@ -53,11 +53,13 @@ declare module 'i18next' {
 }
 
 // Normalize single namespace
-type OmitArrayProps<T> = Exclude<T, keyof any[]>;
-type AppendKeys<K1, K2> = `${K1 & string}.${OmitArrayProps<K2> & string}`;
+type AppendKeys<K1, K2> = `${K1 & string}.${K2 & string}`;
+type AppendKeys2<K1, K2> = `${K1 & string}.${Exclude<K2, keyof any[]> & string}`;
 type Normalize2<T, K = keyof T> = K extends keyof T
-  ? T[K] extends object
-    ? AppendKeys<K, keyof T[K]> | AppendKeys<K, Normalize2<T[K]>>
+  ? T[K] extends Record<string, any>
+    ? T[K] extends readonly any[]
+      ? AppendKeys2<K, keyof T[K]> | AppendKeys2<K, Normalize2<T[K]>>
+      : AppendKeys<K, keyof T[K]> | AppendKeys<K, Normalize2<T[K]>>
     : never
   : never;
 type Normalize<T> = keyof T | Normalize2<T>;
