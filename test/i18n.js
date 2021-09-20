3ce79c812d47e77eaeb9579fd1a968a29bf84e70
Adriano Raiano <adriano@raiano.ch>
2021-09-20 16:09:09 +0200

    update i18next dependency

--- test/i18n.js
+++ test/i18n.js
@@ -23,24 +23,25 @@ i18n.init({
           '<strong>Go</strong><video /><script>console.warn("test")</script> there.',
         transTest2:
           'Hello <1><0>{{name}}</0></1>, you have <3>{{count}}</3> message. Open <5>hear</5>.',
-        transTest2_plural:
+        transTest2_other:
           'Hello <1><0>{{name}}</0></1>, you have <3>{{count}}</3> messages. Open <5>here</5>.',
         transTest2InV2: 'Hello <1>{{name}}</1>, you have {{count}} message. Open <5>hear</5>.',
-        transTest2InV2_plural:
+        transTest2InV2_other:
           'Hello <1>{{name}}</1>, you have {{count}} messages. Open <5>here</5>.',
         testTransKey1: '<0>{{numOfItems}}</0> item matched.',
-        testTransKey1_plural: '<0>{{numOfItems}}</0> items matched.',
+        testTransKey1_other: '<0>{{numOfItems}}</0> items matched.',
         testTransKey2: '<0><0>{{numOfItems}}</0></0> item matched.',
-        testTransKey2_plural: '<0><0>{{numOfItems}}</0></0> items matched.',
+        testTransKey2_other: '<0><0>{{numOfItems}}</0></0> items matched.',
         testTransKey3: 'Result: <1><0>{{numOfItems}}</0></1> item matched.',
-        testTransKey3_plural: 'Result: <1><0>{{numOfItems}}</0></1> items matched.',
+        testTransKey3_other: 'Result: <1><0>{{numOfItems}}</0></1> items matched.',
         testInvalidHtml: '<hello',
         testInvalidHtml2: '<hello>',
         testTrans4KeyWithNestedComponent: 'Result should be a list: <0></0>',
         testTrans5KeyWithNestedComponent: 'Result should be a list: <1></1>',
         testTrans5KeyWithValue: 'Result should be rendered within tag <0>{{testValue}}</0>',
-		transTest3: 'Result should be a clickable link <0 href="https://www.google.com">Google</0>',
-		transTest3_overwrite: 'Result should be a clickable link <0 href="https://www.google.com">Google</0>',
+        transTest3: 'Result should be a clickable link <0 href="https://www.google.com">Google</0>',
+        transTest3_overwrite:
+          'Result should be a clickable link <0 href="https://www.google.com">Google</0>',
       },
       other: {
         transTest1: 'Another go <1>there</1>.',
