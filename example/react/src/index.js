44655851e10c900d1a70d52df6eed547da508847
Adriano Raiano <adriano@raiano.ch>
2022-04-13 09:30:11 +0200

    update react example

--- example/react/src/index.js
+++ example/react/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
 // import i18n (needs to be bundled ;))
 import './i18n';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
