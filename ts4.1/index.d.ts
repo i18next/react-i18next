f59657caa0c2067f4f5bbc275b6914e3b9cab685
vovsemenv <20096510+vovsemenv@users.noreply.github.com>
2022-06-03 07:34:36 +0300

    fix Translation component types (#1509)

--- ts4.1/index.d.ts
+++ ts4.1/index.d.ts
@@ -326,8 +326,11 @@ export function withSSR(): <Props>(
   getInitialProps: (ctx: unknown) => Promise<any>;
 };
 
-export interface WithTranslation<N extends Namespace = DefaultNamespace> {
-  t: TFunction<N>;
+export interface WithTranslation<
+  N extends Namespace = DefaultNamespace,
+  TKPrefix extends KeyPrefix<N> = undefined
+> {
+  t: TFunction<N, TKPrefix>;
   i18n: i18n;
   tReady: boolean;
 }
@@ -337,10 +340,14 @@ export interface WithTranslationProps {
   useSuspense?: boolean;
 }
 
-export function withTranslation<N extends Namespace = DefaultNamespace>(
+export function withTranslation<
+  N extends Namespace = DefaultNamespace,
+  TKPrefix extends KeyPrefix<N> = undefined
+>(
   ns?: N,
   options?: {
     withRef?: boolean;
+    keyPrefix?: TKPrefix;
   },
 ): <
   C extends React.ComponentType<React.ComponentProps<any> & WithTranslationProps>,
@@ -361,9 +368,9 @@ export interface I18nextProviderProps {
 export const I18nextProvider: React.FunctionComponent<I18nextProviderProps>;
 export const I18nContext: React.Context<{ i18n: i18n }>;
 
-export interface TranslationProps<N extends Namespace = DefaultNamespace> {
+export interface TranslationProps<N extends Namespace = DefaultNamespace, TKPrefix> {
   children: (
-    t: TFunction<N>,
+    t: TFunction<N, TKPrefix>,
     options: {
       i18n: i18n;
       lng: string;
@@ -373,8 +380,10 @@ export interface TranslationProps<N extends Namespace = DefaultNamespace> {
   ns?: N;
   i18n?: i18n;
   useSuspense?: boolean;
+  keyPrefix?: TKPrefix;
 }
 
-export function Translation<N extends Namespace = DefaultNamespace>(
-  props: TranslationProps<N>,
-): any;
+export function Translation<
+  N extends Namespace = DefaultNamespace,
+  TKPrefix extends KeyPrefix<N> = undefined
+>(props: TranslationProps<N, TKPrefix>): any;
