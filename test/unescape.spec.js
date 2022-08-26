052784d8349cc6d80bd149f084b4d69c4e2f2fb9
amagee-willow <94665616+amagee-willow@users.noreply.github.com>
2022-08-26 15:14:01 +1000

    support unescaping forward slash (#1548)
    Co-authored-by: Andrew Magee <amagee@gmail.com>

--- test/unescape.spec.js
+++ test/unescape.spec.js
@@ -3,9 +3,9 @@ import { unescape } from '../src/unescape';
 describe('unescape', () => {
   it('should correctly unescape', () => {
     const unescaped = unescape(
-      '&amp; &#38; &lt; &#60; &gt; &#62; &apos; &#39; &quot; &#34; &nbsp; &#160; &copy; &#169; &reg; &#174; &hellip; &#8230;',
+      '&amp; &#38; &lt; &#60; &gt; &#62; &apos; &#39; &quot; &#34; &nbsp; &#160; &copy; &#169; &reg; &#174; &hellip; &#8230; &#x2F;',
     );
 
-    expect(unescaped).toEqual('& & < < > > \' \' " "     © © ® ® … …');
+    expect(unescaped).toEqual('& & < < > > \' \' " "     © © ® ® … … /');
   });
 });
