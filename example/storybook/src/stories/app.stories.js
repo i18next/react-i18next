74138934bc39794f819e8d635d74b6116b0c110c
Shaun Lloyd <contactshaunlloyd+github@gmail.com>
2022-08-30 11:03:41 -0400

    Update storybook example  (#1551)
    * Update storybook example to use CSF
    
    * Update README

--- example/storybook/src/stories/app.stories.js
+++ example/storybook/src/stories/app.stories.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import '../i18n';
 import App from '../App';
 
 // for this case we have set i18n.use(initReactI18next) in '../src/i18n' so no I18nextProvicer is needed to pass i18n down via context api
+export default {
+  title: 'Full App',
+  component: App,
+};
 
-storiesOf('App with initReactI18next', module).add('app view', () => <App />);
+export const Default = () => <App />;
 
 // learn more:
 // https://react.i18next.com/components/overview#getting-the-t-function
