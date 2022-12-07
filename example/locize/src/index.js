0d5bd6e2ee96662f23c0b8ca45974f2fdd0f0076
Adriano Raiano <adriano@raiano.ch>
2022-12-07 11:52:41 +0100

    update some examples

--- example/locize/src/index.js
+++ example/locize/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
 // import i18n (needs to be bundled ;))
 import './i18n';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
