7f76aa9a10ec53245504f9b6bdc44e68c33c0e75
Adriano Raiano <adriano@raiano.ch>
2023-02-14 14:51:51 +0100

    jest example: if no children, show i18nKey

--- example/test-jest/src/__mocks__/react-i18next.js
+++ example/test-jest/src/__mocks__/react-i18next.js
@@ -37,7 +37,7 @@ useMock.i18n = {};
 module.exports = {
   // this mock makes sure any components using the translate HoC receive the t function as a prop
   withTranslation: () => Component => props => <Component t={k => k} {...props} />,
-  Trans: ({ children }) =>
+  Trans: ({ children, i18nKey }) => !children ? i18nKey :
     Array.isArray(children) ? renderNodes(children) : renderNodes([children]),
   Translation: ({ children }) => children(k => k, { i18n: {} }),
   useTranslation: () => useMock,
