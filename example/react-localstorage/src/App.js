c53f109f9541d0fb891b67d423fdf052f5ffed6c
Adriano Raiano <adriano@raiano.ch>
2023-06-08 10:44:02 +0200

    example: react-localstorage

---
+++ example/react-localstorage/src/App.js
@@ -0,0 +1,67 @@
+import React, { Component, Suspense } from 'react';
+import { useTranslation, withTranslation, Trans } from 'react-i18next';
+import logo from './logo.svg';
+import './App.css';
+
+// use hoc for class based components
+class LegacyWelcomeClass extends Component {
+  render() {
+    const { t } = this.props;
+    return <h2>{t('title')}</h2>;
+  }
+}
+const Welcome = withTranslation()(LegacyWelcomeClass);
+
+// Component using the Trans component
+function MyComponent() {
+  return (
+    <Trans i18nKey="description.part1">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </Trans>
+  );
+}
+
+// page uses the hook
+function Page() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Welcome />
+        <button type="button" onClick={() => changeLanguage('de')}>
+          de
+        </button>
+        <button type="button" onClick={() => changeLanguage('en')}>
+          en
+        </button>
+      </div>
+      <div className="App-intro">
+        <MyComponent />
+      </div>
+      <div>{t('description.part2')}</div>
+    </div>
+  );
+}
+
+// loading component for suspense fallback
+const Loader = () => (
+  <div className="App">
+    <img src={logo} className="App-logo" alt="logo" />
+    <div>loading...</div>
+  </div>
+);
+
+// here app catches the suspense from page in case translations are not yet loaded
+export default function App() {
+  return (
+    <Suspense fallback={<Loader />}>
+      <Page />
+    </Suspense>
+  );
+}
--- example/react-localstorage/src/i18n.js
+++ example/react-localstorage/src/i18n.js
@@ -1,12 +1,14 @@
 import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
+import HttpBackend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
+import ChainedBackend from 'i18next-chained-backend';
+import LocalStorageBackend from 'i18next-localstorage-backend';
 
 i18n
   // load translation using http -> see /public/locales
   // learn more: https://github.com/i18next/i18next-http-backend
-  .use(Backend)
+  .use(ChainedBackend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
@@ -17,6 +19,16 @@ i18n
   .init({
     fallbackLng: 'en',
     debug: true,
+
+    backend: {
+      backends: [LocalStorageBackend, HttpBackend],
+      backendOptions: [],
+      cacheHitMode: 'refreshAndUpdateStore',
+    },
+
+    react: {
+      bindI18nStore: 'added', // this way, when the HttpBackend delivers new translations (thanks to refreshAndUpdateStore), the UI gets updated
+    },
   });
 
 export default i18n;
