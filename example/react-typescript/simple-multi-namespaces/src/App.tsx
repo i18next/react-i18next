82fe57242e34dc8a1104f21966db431e54595aad
Adriano Raiano <adriano@raiano.ch>
2023-06-15 14:01:13 +0200

    fix ts examples

--- example/react-typescript/simple-multi-namespaces/src/App.tsx
+++ example/react-typescript/simple-multi-namespaces/src/App.tsx
@@ -1,14 +1,16 @@
 import './i18n/config';
-import { useTranslation } from 'react-i18next';
+import Comp1 from './components/Comp1';
+import Comp2 from './components/Comp2';
+import Comp3 from './components/Comp3';
 
 function App() {
-  const {t} = useTranslation();
-
   return (
     <div className="App">
-      <p>{t('title')}</p>
-      <p>{t('description.part1')}</p>
-      <p>{t('description.part2')}</p>
+      <Comp1 />
+      <hr />
+      <Comp2 />
+      <hr />
+      <Comp3 />
     </div>
   );
 }
--- example/react-typescript/simple-multi-namespaces/src/components/Comp3.tsx
+++ example/react-typescript/simple-multi-namespaces/src/components/Comp3.tsx
@@ -1,12 +1,12 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
 function Comp2() {
-  const {t} = useTranslation(['ns1', 'ns2']);
+  const { t } = useTranslation(['ns1', 'ns2']);
 
   return (
     <div className="App">
-      <p>{t('ns1:description.part1')}</p>
-      <p>{t('ns2:description.part2')}</p>
+      <p>{t('description.part1', { ns: 'ns1' })}</p>
+      <p>{t('description.part2', { ns: 'ns2' })}</p>
     </div>
   );
 }
