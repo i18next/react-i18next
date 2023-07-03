e6cb106a134ddc1e21c5cb69fedbab466f4b9223
Adriano Raiano <adriano@raiano.ch>
2023-07-03 11:06:09 +0200

    update ts example deps

--- example/react-typescript/simple/src/App.tsx
+++ example/react-typescript/simple/src/App.tsx
@@ -1,14 +1,19 @@
 import './i18n/config';
-import { useTranslation } from 'react-i18next';
+import { useTranslation, Trans } from 'react-i18next';
 
 function App() {
   const { t } = useTranslation();
+
+  const count = 3;
 
   return (
     <div className="App">
       <p>{t('title', { name: 'John' })}</p>
       <p>{t('description.part1')}</p>
       <p>{t('description.part2')}</p>
+      <Trans i18nKey="userMessagesUnread" count={count}>
+        You have {{ count }} unread message.
+      </Trans>
     </div>
   );
 }
