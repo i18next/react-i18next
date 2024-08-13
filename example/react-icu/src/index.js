8276a35fb29ce6c798faec23f6757a84e7b6a273
Ricardo Costa <48949546+ripecosta@users.noreply.github.com>
2024-08-13 16:05:53 +0100

    chore: update example dependencies (#1788)

--- example/react-icu/src/index.js
+++ example/react-icu/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import './i18n';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
